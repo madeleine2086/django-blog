534123cfd1de2c1a09bf5b89aad7c48a148481dc
Magda Dmitruk <dmitrukm@gmail.com>
2024-02-24 17:18:37 +0000

    filter posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-madeleine208-djangoblog-gshdo2bvn29.ws-eu108.gitpod.io','.herokuapp.com']
 
