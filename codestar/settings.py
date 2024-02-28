68094b68158f30f387a6996885646951ea7db395
Magda Dmitruk <dmitrukm@gmail.com>
2024-02-28 18:30:35 +0000

    add post detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-madeleine208-djangoblog-gshdo2bvn29.ws-eu108.gitpod.io','.herokuapp.com']
 
